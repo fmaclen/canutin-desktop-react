744d2bff6530ba5040f2fcca8266cbb52843b9fb
Fernando Maclen <fmaclen@gmail.com>
2021-06-22 13:26:23 -0400

    Remove unused useState

--- src/app/components/common/StatusBar/index.tsx
+++ src/app/components/common/StatusBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import useBreadcrumbs from 'use-react-router-breadcrumbs';
 import styled from 'styled-components';
 
