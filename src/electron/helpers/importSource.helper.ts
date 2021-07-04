99c8f08af0c3dee7c62f240886582b894d320494
Nicolas Santos <hi@nicolassantos.dev>
2021-07-03 22:55:39 -0300

    Update message when the canutin file is imported from wizard form

--- src/electron/helpers/importSource.helper.ts
+++ src/electron/helpers/importSource.helper.ts
@@ -45,10 +45,11 @@ export const analyzeCanutinFile = async (filePath: string, win: BrowserWindow | 
   try {
     const canutinFile = JSON.parse(file);
     const hasCanutinFileAccounts = canutinFile?.accounts?.length > 0;
+    const countAssets = canutinFile?.assets?.length;
 
-    if (hasCanutinFileAccounts) {
-      const countAccounts = canutinFile.accounts.length;
-      const countTransactions = canutinFile.accounts.reduce(
+    if (hasCanutinFileAccounts || countAssets) {
+      const countAccounts = canutinFile?.accounts?.length;
+      const countTransactions = canutinFile?.accounts?.reduce(
         (countTransactions: number, account: { transactions: CanutinFileTransactionType[] }) => {
           if (account?.transactions) {
             return countTransactions + account.transactions.length;
@@ -65,6 +66,7 @@ export const analyzeCanutinFile = async (filePath: string, win: BrowserWindow | 
         metadata: {
           countAccounts,
           countTransactions,
+          countAssets,
         },
       });
     } else {
