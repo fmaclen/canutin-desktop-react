7cccf72dfb253792eb43549c1b555572cce14a16
Fernando Maclen <fmaclen@gmail.com>
2021-07-28 18:33:08 -0400

    Update index.tsx

--- src/app/components/AccountAsset/ImportWizardForm/OtherCSVForm/index.tsx
+++ src/app/components/AccountAsset/ImportWizardForm/OtherCSVForm/index.tsx
@@ -186,7 +186,7 @@ const OtherCSVForm = ({ data, metadata }: OtherCSVFormProps) => {
       setError('dateFormat', {
         type: 'manual',
         message:
-          'Couldn’t read the transaction dates, try another date format or update the file manually',
+          'Couldn’t interprete the transaction dates, try another date format or update the file manually',
       });
     }
 
