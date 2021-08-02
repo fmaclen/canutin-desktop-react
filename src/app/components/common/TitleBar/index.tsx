c3bba9fe783ace60ee3a87400d74637e5230003a
Fernando Maclen <fmaclen@gmail.com>
2021-08-02 10:03:18 -0400

    Add basic component structure for window controls

--- src/app/components/common/TitleBar/index.tsx
+++ src/app/components/common/TitleBar/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import { ReactComponent as Icon } from '@assets/icons/CanutinIcon.svg';
 
-import { container, icon } from './styles';
+import { container, icon, windowControls, minimize, maximize, close } from './styles';
 
 const Container = styled.div`
   ${container}
@@ -11,10 +11,39 @@ const Container = styled.div`
 const CanutinIcon = styled(Icon)`
   ${icon}
 `;
+const WindowControls = styled.nav`
+  ${windowControls}
+`
+const Minimize = styled.button`
+  ${minimize}
+`
+const Maximize = styled.button`
+  ${maximize}
+`
+const Close = styled.button`
+  ${close}
+`
+
+const isMacOs = () => {
+  switch (process.platform) {
+    case "win32":
+    case "linux":
+      return false;
+    default:
+      return true;
+  }
+} 
 
 const TitleBar = () => (
   <Container>
     <CanutinIcon />
+    {!isMacOs() && (
+      <WindowControls>
+        <Minimize />
+        <Maximize maximized={false} />
+        <Close />
+      </WindowControls>
+    )}
   </Container>
 );
 
--- src/app/components/common/TitleBar/styles.ts
+++ src/app/components/common/TitleBar/styles.ts
@@ -1,6 +1,6 @@
 import { css } from 'styled-components';
 
-import { whitePlain, grey10 } from '@appConstants/colors';
+import { whitePlain, grey10, grey5 } from '@appConstants/colors';
 
 export const container = css`
   background-color: ${whitePlain};
@@ -9,6 +9,7 @@ export const container = css`
   display: grid;
   grid-auto-flow: column;
   grid-gap: 32px;
+  justify-content: space-between;
 
   -webkit-app-region: drag;
   -webkit-user-select: none;
@@ -17,7 +18,35 @@ export const container = css`
 export const icon = css`
   align-self: center;
   border-left: 1px solid ${grey10};
-  margin-left: auto;
   height: 16px;
   padding: 16px;
 `;
+
+export const windowControls = css`
+  display: grid;
+  grid-auto-flow: column;
+`
+
+const baseControl = css`
+  border: none;
+  padding-left: 40px;
+  padding-right: 40px;
+  height: 100%;
+  background-color: ${whitePlain};
+
+  &:hover {
+    background-color: ${grey5};
+  }
+`
+
+export const minimize = css`
+  ${baseControl}
+`
+
+export const maximize = css<{maximized: boolean}>`
+  ${baseControl}
+`
+  
+export const close = css`
+  ${baseControl}
+`
