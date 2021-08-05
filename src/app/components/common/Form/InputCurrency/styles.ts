1db8afa31e60404a5e98e603698b7b02f24325dd
Fernando Maclen <fmaclen@gmail.com>
2021-08-04 23:26:27 -0400

    Add placeholder styles

--- src/app/components/common/Form/InputCurrency/styles.ts
+++ src/app/components/common/Form/InputCurrency/styles.ts
@@ -1,12 +1,13 @@
 import { css } from 'styled-components';
 import { greenPlain, grey10, redPlain, whitePlain } from '@appConstants/colors';
 import { monospaceRegular } from '@appConstants/fonts';
-import { inputShared } from '@appConstants/inputs';
+import { inputShared, placeholder } from '@appConstants/inputs';
 import { TransactionTypesEnum } from '@appConstants/misc';
 
 export const inputElement = css<{ disabled?: boolean; transactionType?: TransactionTypesEnum }>`
   ${inputShared};
   ${monospaceRegular};
+  ${placeholder};
   min-height: 40px;
 
   background-color: ${whitePlain};
