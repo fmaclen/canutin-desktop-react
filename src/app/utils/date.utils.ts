1a34e595a3bdcd9e917c37d7cf6b20ae20902d6f
Fernando Maclen <fmaclen@gmail.com>
2021-08-07 00:26:50 -0400

    Cleanup formatDate

--- src/app/utils/date.utils.ts
+++ src/app/utils/date.utils.ts
@@ -1,15 +1,13 @@
 import { format } from 'date-fns';
 
-const DATE_FORMAT = 'MMM dd, yyyy';
-
 export const dateInUTC = (date: Date) => {
   return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0));
 };
 
-const formatForUserTimezone = (date: Date) => {
-  return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0);
-};
-
 export const formatDate = (date: Date) => {
-  return format(formatForUserTimezone(date), DATE_FORMAT);
+  const DATE_FORMAT = 'MMM dd, yyyy';
+  const dateToUserTimezone = () =>
+    new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0);
+
+  return format(dateToUserTimezone(), DATE_FORMAT);
 };
