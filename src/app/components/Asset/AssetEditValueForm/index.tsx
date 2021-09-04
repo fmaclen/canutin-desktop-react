0d792f9a46d61f973dba0a9bcc8c38978b089c0a
Fernando Maclen <fmaclen@gmail.com>
2021-09-04 11:26:02 -0400

    Update order of fields and help notices

--- src/app/components/Asset/AssetEditValueForm/index.tsx
+++ src/app/components/Asset/AssetEditValueForm/index.tsx
@@ -95,26 +95,26 @@ const AssetEditValueForm = ({ asset }: AssetEditValueFormProps) => {
             />
           </>
         )}
+        <InputCurrencyField
+          label="Value"
+          name="value"
+          allowNegative={!shouldDisplayAssetWithSymbolFields}
+          control={control}
+          disabled={shouldDisplayAssetWithSymbolFields}
+          rules={{ required: !shouldDisplayAssetWithSymbolFields }}
+        />
         {lastBalanceStatement?.sold === false && (
           <FieldNotice
             title="Value history"
             description={
               <div>
-                Every time you manually update the quantity or price of an asset, a new balance
-                statement will be created (or updated) for the current week period. You can see past
+                Every time you manually update the value, quantity or price of an asset, a new
+                balance statement will be created for the current week period. You can see past
                 balance statements in the “Overview” tab.
               </div>
             }
           />
         )}
-        <InputCurrencyField
-          label="Value"
-          name="value"
-          allowNegative={false}
-          control={control}
-          disabled={shouldDisplayAssetWithSymbolFields}
-          rules={{ required: !shouldDisplayAssetWithSymbolFields }}
-        />
       </Fieldset>
       <Fieldset>
         <Field label="Mark asset as" name="sold">
