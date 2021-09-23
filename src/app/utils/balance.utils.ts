8e7016037bdccb27e9d67215fa718dda2d00cd12
Nicolas Santos <nicolassantos16@gmail.com>
2021-09-23 02:28:35 -0300

    Add trailingcashflow panel to the big picture

--- src/app/utils/balance.utils.ts
+++ src/app/utils/balance.utils.ts
@@ -7,6 +7,7 @@ import {
   endOfWeek,
   max,
   sub,
+  eachMonthOfInterval,
 } from 'date-fns';
 import merge from 'deepmerge';
 
@@ -19,6 +20,7 @@ import {
 } from '@database/entities';
 import { BalanceData, AccountAssetBalance } from '@components/BalanceSheet/BalancesByGroup';
 import { BalanceGroupEnum } from '@enums/balanceGroup.enum';
+import { TrailingCashflowSegmentsEnum } from '@app/components/BigPicture/TrailingCashflow';
 
 export const getBalanceForAssetByBalanceGroup = (assets: Asset[]) => {
   const assetsNoSold = assets.filter(
@@ -162,6 +164,86 @@ export const getSelectedTransactions = (transactions: Transaction[], from: Date,
   return transactions.filter(
     transaction => isBefore(from, transaction.date) && isAfter(to, transaction.date)
   );
+};
+
+export type TransactionsTrailingCashflowType = {
+  month: Date;
+  income: number;
+  expenses: number;
+  surplus: number;
+  id: number;
+};
+
+export const getTransactionsTrailingCashflow = (transactions: Transaction[]) => {
+  if (transactions.length === 0) {
+    return [];
+  }
+
+  const monthDates = eachMonthOfInterval({
+    start: transactions[transactions.length - 1].date,
+    end: transactions[0].date,
+  });
+
+  return monthDates.reduce((acc: TransactionsTrailingCashflowType[], monthDate, index) => {
+    const monthlyTransactions = getSelectedTransactions(
+      transactions,
+      monthDate,
+      monthDates[index + 1] ? monthDates[index + 1] : new Date()
+    );
+    const income = monthlyTransactions.reduce(
+      (acc, transaction) => (transaction.amount > 0 ? transaction.amount + acc : acc),
+      0
+    );
+    const expenses = monthlyTransactions.reduce(
+      (acc, transaction) => (transaction.amount < 0 ? transaction.amount + acc : acc),
+      0
+    );
+    const surplus = expenses + income;
+
+    return [
+      ...acc,
+      {
+        month: monthDate,
+        income,
+        expenses,
+        surplus,
+        id: index,
+      },
+    ];
+  }, []);
+};
+
+export const getTransactionTrailingCashflowAverage = (
+  trailingCashflow: TransactionsTrailingCashflowType[],
+  option: TrailingCashflowSegmentsEnum
+) => {
+  if (option === TrailingCashflowSegmentsEnum.LAST_12_MONTHS) {
+    const monthLimit = sub(new Date(), { months: 12 });
+
+    return trailingCashflow
+      .filter(({ month }) => isAfter(monthLimit, month))
+      .reduce(
+        (acc, { expenses, income, surplus }) => {
+          return [acc[0] + (income / 12), acc[1] + (expenses / 12), acc[2] + (surplus / 12)];
+        },
+        [0, 0, 0]
+      );
+  }
+
+  if (option === TrailingCashflowSegmentsEnum.LAST_6_MONTHS) {
+    const monthLimit = sub(new Date(), { months: 6 });
+
+    return trailingCashflow
+      .filter(({ month }) => isAfter(monthLimit, month))
+      .reduce(
+        (acc, { expenses, income, surplus }) => {
+          return [acc[0] + (income / 6), acc[1] + (expenses / 6), acc[2] + (surplus / 6)];
+        },
+        [0, 0, 0]
+      );
+  }
+
+  return [0, 0, 0];
 };
 
 export const getSelectedBalanceStatements = (
