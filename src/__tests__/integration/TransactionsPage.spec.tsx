062ac89bdd066b888bddb1422bfa286886107c8d
Fernando Maclen <fmaclen@gmail.com>
2021-12-19 11:23:25 -0300

    Add more assertions for Transaction page

--- src/__tests__/integration/TransactionsPage.spec.tsx
+++ src/__tests__/integration/TransactionsPage.spec.tsx
@@ -9,7 +9,11 @@ import { endOfDay, startOfDay } from 'date-fns';
 import 'regenerator-runtime/runtime';
 
 import { dateInUTC } from '@app/utils/date.utils';
-import { DB_GET_ACCOUNTS_ACK, FILTER_TRANSACTIONS_ACK } from '@constants/events';
+import {
+  DB_GET_ACCOUNTS_ACK,
+  FILTER_TRANSACTIONS_ACK,
+  FILTER_TRANSACTIONS,
+} from '@constants/events';
 import { DATABASE_CONNECTED } from '@constants';
 import { AppCtxProvider } from '@app/context/appContext';
 import { EntitiesProvider } from '@app/context/entitiesContext';
@@ -20,6 +24,7 @@ import { accountCheckingDetails } from '@database/seed/demoData/accounts';
 import { TransactionsProvider } from '@app/context/transactionsContext';
 import { accountCheckingTransactionSet } from '@database/seed/demoData/transactions';
 import { filters } from '@app/constants/filters';
+import mapCategories from '@database/helpers/importResources/mapCategories';
 
 const initAppWithContexts = () => {
   render(
@@ -97,7 +102,7 @@ describe('Transactions tests', () => {
       .map(transaction => ({
         ...transaction,
         account: { ...accountCheckingDetails },
-        category: { name: transaction.categoryName },
+        category: { name: mapCategories(transaction.categoryName) },
       }));
 
     mocked(ipcRenderer).on.mockImplementation((event, callback) => {
@@ -133,20 +138,110 @@ describe('Transactions tests', () => {
     expect(cardTransactions).toHaveTextContent('Transactions');
     expect(cardTransactions).toHaveTextContent('8');
 
-    const cardNetBalance = screen.getByTestId('card-net-balance');
+    let cardNetBalance = screen.getByTestId('card-net-balance');
     expect(cardNetBalance).toHaveTextContent('Net balance');
     expect(cardNetBalance).toHaveTextContent('$350');
     expect(cardNetBalance).not.toHaveTextContent('-$350');
 
-    // TODO
-    // - assert number of rows generated
-    // - assert the first transaction values
-    // - assert the last transaction values
-    // - assert sorting works
-    // - assert credit/debit segmented control works
-    // - assert typing in the search bar filters transactions
-    // - assert switching date filters function is called (?)
+    let rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions.length).toBe(8);
+    expect(rowTransactions[0]).toHaveTextContent('Toyota - TFS Payment');
+    expect(rowTransactions[0]).toHaveTextContent('Automotive');
+    expect(rowTransactions[0]).toHaveTextContent("Alice's Checking");
+    expect(rowTransactions[0]).toHaveTextContent('-$500');
+    expect(rowTransactions[7]).toHaveTextContent('Westside Apartments');
+    expect(rowTransactions[7]).toHaveTextContent('Housing');
+    expect(rowTransactions[7]).toHaveTextContent("Alice's Checking");
+    expect(rowTransactions[7]).toHaveTextContent('-$2,250');
+
+    const tableHeaderDate = screen.getByText('Date');
+    const tableHeaderAmount = screen.getByText('Amount');
+    expect(tableHeaderDate).toBeVisible();
+    expect(tableHeaderAmount).toBeVisible();
+    expect(screen.getByText('Description')).toBeVisible();
+    expect(screen.getByText('Category')).toBeVisible();
+    expect(screen.getByText('Account')).toBeVisible();
+
+    // Sort by "Date" (ascending)
+    userEvent.click(tableHeaderDate);
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions[0]).toHaveTextContent('Westside Apartments');
+    expect(rowTransactions[0]).toHaveTextContent('-$2,250');
+    expect(rowTransactions[0]).not.toHaveTextContent('Toyota - TFS Payment');
+
+    // Sort by "Amount" (ascending)
+    userEvent.click(tableHeaderAmount);
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions[0]).toHaveTextContent('-$2,250');
+    expect(rowTransactions[0]).not.toHaveTextContent('$2,800');
+
+    // Sort by "Amount" (descending)
+    userEvent.click(tableHeaderAmount);
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions[0]).toHaveTextContent('$2,800');
+    expect(rowTransactions[0]).not.toHaveTextContent('-$2,250');
+    expect(rowTransactions[7]).toHaveTextContent('-$2,250');
+
+    // Filter by "Credits"
+    userEvent.click(screen.getByText(/Credits/i));
+    cardNetBalance = screen.getByTestId('card-net-balance');
+    expect(cardNetBalance).toHaveTextContent('$5,600');
+
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions.length).toBe(3);
+    expect(rowTransactions[0]).toHaveTextContent('$2,800');
+    expect(rowTransactions[2]).toHaveTextContent('$0');
+    expect(rowTransactions[2]).not.toHaveTextContent('-$2,250');
+
+    // Filter by "Debits"
+    userEvent.click(screen.getByText(/Debits/i));
+    cardNetBalance = screen.getByTestId('card-net-balance');
+    expect(cardNetBalance).toHaveTextContent('-$5,250');
+
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions.length).toBe(5);
+    expect(rowTransactions[0]).toHaveTextContent('-$250');
+    expect(rowTransactions[0]).not.toHaveTextContent('$2,800');
+    expect(rowTransactions[4]).toHaveTextContent('-$2,250');
+
+    // Filter by "All"
+    userEvent.click(screen.getByText(/All/i));
+    cardNetBalance = screen.getByTestId('card-net-balance');
+    expect(cardNetBalance).toHaveTextContent('$350');
+
+    rowTransactions = screen.getAllByTestId('row-transaction');
+    expect(rowTransactions.length).toBe(8);
+    expect(rowTransactions[0]).toHaveTextContent('$2,800');
+    expect(rowTransactions[7]).toHaveTextContent('-$2,250');
+
+    // Filter by keyword
+    const inputSearch = screen.getByPlaceholderText(
+      'Type to filter by date, description, category, account or amount'
+    );
+    userEvent.type(inputSearch, 'transfer to');
+    setTimeout(() => {
+      rowTransactions = screen.getAllByTestId('row-transaction');
+      expect(rowTransactions.length).toBe(2);
+    }, 200);
+
+    userEvent.click(screen.getByText('Last 3 months'));
+    expect(screen.getByText('This month')).toBeVisible();
+    expect(screen.getByText('Last 6 months')).toBeVisible();
+    expect(screen.getByText('Last 12 months')).toBeVisible();
+    expect(screen.getByText('Year to date')).toBeVisible();
+    expect(screen.getByText('Last year')).toBeVisible();
+    expect(screen.getByText('Lifetime')).toBeVisible();
+
+    userEvent.click(screen.getByText('Last 6 months'));
+    const spyOnIpcRenderer = jest.spyOn(ipcRenderer, 'send');
+    const { dateFrom, dateTo } = filters.find(filter => {
+      return filter.label === 'Last 6 months';
+    });
+    expect(spyOnIpcRenderer).toBeCalledWith(FILTER_TRANSACTIONS, {
+      dateFrom: dateFrom,
+      dateTo: dateTo,
+    });
   });
 
-  // - Add test for "add transaction" flow
+  // TODO: add test for "add transaction" flow
 });
